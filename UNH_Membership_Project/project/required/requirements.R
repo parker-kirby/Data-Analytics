33f57d60cf1e5b62b3f1612d1111c8af57a69db8
parker-kirby <107413553+parker-kirby@users.noreply.github.com>
2022-06-14 12:11:59 -0400

    Delete UNH_Membership_Project directory

--- UNH_Membership_Project/project/required/requirements.R
+++
@@ -1,4 +0,0 @@
-install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("optparse",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("caret") #http://topepo.github.io/caret/index.html
-
