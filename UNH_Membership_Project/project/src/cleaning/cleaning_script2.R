f22e76cdb0d4cb03e900fba5c3378dbf84181fef
parker-kirby <pkirby@iapp.org>
2022-06-23 10:37:45 -0400

    updates
    updates

--- UNH_Membership_Project/project/src/cleaning/cleaning_script2.R
+++ UNH_Membership_Project/project/src/cleaning/cleaning_script2.R
@@ -12,7 +12,7 @@ data <- data[data$`Membership Category` == "Individual"]
 data <- data[data$Status == "Active"]
 data <- data[data$`Member Type` != "Individual Green Membership"]
 data <- data[data$`Member Type` != "Press"]
-data <- data[data$`Product: Record Type` != "Merchandise"] 
+#data <- data[data$`Product: Record Type` != "Merchandise"] 
 
 #Converting date formats and specifying our date range
 data$`Created Date`<- as.Date(data$`Created Date`, "%m/%d/%Y")
@@ -28,13 +28,23 @@ remove <- c("OTP", "Designation", "Default Country", "Product: Category", "Produ
 data <- data[, remove:=NULL, with=FALSE]
 
 
-##############TESTING###################
+###############TESTING###################
 
 unique(data$`Product: Record Type`)
-data1 <- data[data$`Order Id` == "Order 0105201"]
-data2 <- data[data$`Order Id` == "Order 0199073"]
+data1 <- data[data$`Order Id` == "Order 0222749"]
+data2 <- data[data$`Order Id` == "Order 0105183"]
+data3 <- data[data$`Product: Record Type` == "Donation"]
+data4 <- data[data$`Unique ID` == "34248"]
 
-########################################
+#########################################
+data <- data[data$`Product: Record Type` == "Coupon" & data$`Unit Price` < 0]
 
+dt1 <- data[duplicated(data$`Order Id`)]
+dt2 <- dt1[dt1$`Product: Record Type` == "Membership" & dt1$`Unit Price` == 0]
+dt3 <- dt1[dt1$`Product: Record Type` == "Membership" & dt1$`Unit Price` > 0]
+dt4 <- dt1[dt1$`Product: Record Type` == "Membership" & dt1$`Unit Price` < dt1$`Total Payment` & dt1$`Unit Price` > 0]
 
-fwrite()
+#########################################
+
+#writing out our cleaned data
+fwrite(data, './UNH_Membership_Project/project/volume/data/interim/cleaned_data.csv')
