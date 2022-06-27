df086819ec43dead9b404448a281b5d4214adb4a
parker-kirby <pkirby@iapp.org>
2022-06-27 08:56:04 -0400

    hi
    hi

---
+++ UNH_Membership_Project/project/src/cleaning/cleaning_script3.R
@@ -0,0 +1,51 @@
+library(data.table)
+library(dplyr)
+library(carat)
+
+#read in our data
+data <- fread('./UNH_Membership_Project/project/volume/data/raw/raw_data.csv')
+
+#removing records that won't be used for analysis
+data <- distinct(data)
+data <- data[data$`Account Record Type` == "Individual"]
+data <- data[data$`Membership Category` == "Individual"]
+data <- data[data$Status == "Active"]
+data <- data[data$`Member Type` != "Individual Green Membership"]
+data <- data[data$`Member Type` != "Press"]
+data <- data[data$`Product: Record Type` != "Donation"]
+data <- data[data$`Product: Record Type` != "Coupon"]
+
+#Converting date formats and specifying our date range
+data$`Created Date`<- as.Date(data$`Created Date`, "%m/%d/%Y")
+data$`Transaction Date` <- as.Date(data$`Transaction Date`, "%m/%d/%Y")
+
+data <- data[data$`Transaction Date` < as.Date('2022-01-01')]
+
+
+#removing columns that aren't important
+remove <- c("OTP", "Designation", "Default Country", "Product: Category", "Product: Short Name", "Account Safe Id", 
+            "Account Safe Id-1", "Account Record Type-1", "Member-1", "Member Type-1", "Status", 
+            "Person Account: Lead Source", "Certified", "Member")
+data <- data[, remove:=NULL, with=FALSE]
+
+
+###############TESTING###################
+
+unique(data$`Product: Record Type`)
+data_1 <- data[data$`Order Id` == "Order 0107047"]
+data_2 <- data[data$`Order Id` == "Order 0335908"]
+data_3 <- data[data$`Product: Record Type` == "Donation"]
+data_4 <- data[data$`Unique ID` == "275463"]
+
+#########################################
+data <- data[data$`Product: Record Type` == "Coupon" & data$`Unit Price` < 0]
+
+dt1 <- data[duplicated(data$`Order Id`)]
+dt2 <- dt1[dt1$`Product: Record Type` == "Membership" & dt1$`Unit Price` == 0]
+dt3 <- dt1[dt1$`Product: Record Type` == "Membership" & dt1$`Unit Price` > 0]
+dt4 <- dt1[dt1$`Product: Record Type` == "Membership" & dt1$`Unit Price` < dt1$`Total Payment` & dt1$`Unit Price` > 0]
+
+#########################################
+
+#writing out our cleaned data
+fwrite(data, './UNH_Membership_Project/project/volume/data/interim/cleaned_data.csv')
--- UNH_Membership_Project/project/src/main/DirectvsIndirect.R
+++ UNH_Membership_Project/project/src/main/DirectvsIndirect.R
@@ -1,6 +1,6 @@
 #Direct vs Indirect Memberships
 
-data <- fread('./UNH_Membership_Project/project/volume/data/intrim/raw_data.csv')
+data <- fread('./UNH_Membership_Project/project/volume/data/intrim/cleaned_data.csv')
 
 #Defining columns for the different types of membership
 data$direct <- 0
@@ -8,6 +8,35 @@ data$OTP_indirect <- 0
 data$bundle_indirect <- 0
 data$comp_indirect <-0
 
+
+##########################################################################################################
 #Direct membership
-direct_list = c("Professional", "Not-For-Profit", "Government", "Higher Education", "Retired", "Transitional", "Student")
-data$direct[data$`Product: Product Name` %in% direct_list & data$`Unit Price` > 0] <- 1
+data_list <- data[(duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price` == 0]
+
+membership_list = c("Professional", "Not-For-Profit", "Government", "Higher Education", "Retired", "Transitional", "Student")
+#data$direct[data$`Product: Product Name` %in% direct_list & data$`Unit Price` > 0 & data$`Total Payment` >= data$`Unit Price`] <- 1
+data1 <- data[data$`Product: Product Name` %in% membership_list & data$`Unit Price` > 0 & data$`Total Payment` >= data$`Unit Price`]
+
+
+#Get rid of records in data list
+non_use = c(data_list$`Order Id`)
+'%notin%' <- Negate('%in%')
+
+data2 <- data1[data1$`Order Id` %notin% non_use]
+
+data3 <- data2[duplicated(data2$`Order Id`)]
+
+
+##########################################################################################################
+#Indirect OTP
+
+#data$OTP_indirect[duplicated(data$`Order Id`) & data$`Unit Price` == 0 & data$`Total Payment` == 0] <- 1
+
+free_memberships <- data[data$`Product: Product Name` %in% membership_list & data$`Unit Price` == 0 & data$`Total Payment` > 0]
+
+test_indirect <- data[(duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price` == 0 & data$`Total Payment` == 0]
+
+Final_OTP <- test_indirect[test_indirect$`Order Id` %in% free_memberships$`Order Id`]
+
+OTP <- Final_OTP[Final_OTP$`Product: Product Name` %in% membership_list]
+
