49bdc903d5f13f1cbc23333b434c88333472e7bc
parker-kirby <pkirby@iapp.org>
2022-06-27 11:46:04 -0400

    work
    ss

--- UNH_Membership_Project/project/src/main/DirectvsIndirect.R
+++ UNH_Membership_Project/project/src/main/DirectvsIndirect.R
@@ -8,12 +8,12 @@ data$OTP_indirect <- 0
 data$bundle_indirect <- 0
 data$comp_indirect <-0
 
+membership_list = c("Professional", "Not-For-Profit", "Government", "Higher Education", "Retired", "Transitional", "Student")
 
 ##########################################################################################################
 #Direct membership
 data_list <- data[(duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price` == 0]
 
-membership_list = c("Professional", "Not-For-Profit", "Government", "Higher Education", "Retired", "Transitional", "Student")
 #data$direct[data$`Product: Product Name` %in% direct_list & data$`Unit Price` > 0 & data$`Total Payment` >= data$`Unit Price`] <- 1
 data1 <- data[data$`Product: Product Name` %in% membership_list & data$`Unit Price` > 0 & data$`Total Payment` >= data$`Unit Price`]
 
@@ -24,7 +24,7 @@ non_use = c(data_list$`Order Id`)
 
 data2 <- data1[data1$`Order Id` %notin% non_use]
 
-data3 <- data2[duplicated(data2$`Order Id`)]
+data3 <- data2[(duplicated(data2$`Order Id`) | duplicated(data2$`Order Id`, fromLast = TRUE))]
 
 
 ##########################################################################################################
@@ -32,11 +32,25 @@ data3 <- data2[duplicated(data2$`Order Id`)]
 
 #data$OTP_indirect[duplicated(data$`Order Id`) & data$`Unit Price` == 0 & data$`Total Payment` == 0] <- 1
 
-free_memberships <- data[data$`Product: Product Name` %in% membership_list & data$`Unit Price` == 0 & data$`Total Payment` > 0]
+OTP_indirect <- data[(duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price` == 0 & data$`Total Payment` == 0]
 
-test_indirect <- data[(duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price` == 0 & data$`Total Payment` == 0]
+Final_OTP <- test_indirect[test_indirect$`Product: Product Name` %in% membership_list]
 
-Final_OTP <- test_indirect[test_indirect$`Order Id` %in% free_memberships$`Order Id`]
 
-OTP <- Final_OTP[Final_OTP$`Product: Product Name` %in% membership_list]
+#########################################################################################################
+#Indirect Comp
+comped1 <- data[data$`Product: Product Name` %in% membership_list & data$`Unit Price` > 0 & data$`Total Payment` == 0]
+comped2 <- data[data$`Product: Product Name` %in% membership_list & data$`Unit Price` == 0 & data$`Total Payment` == 0 & !(duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE))]
+
+Final_Comp<- rbind(comped1, comped2)
+
+#########################################################################################################
+#Indirect Bundle
+
+bundle1 <- data[data$`Product: Product Name` %in% membership_list & (duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price` == 0 & data$`Total Payment` > 0]
+
+bundle2 <- data[data$`Product: Product Name` %in% membership_list & (duplicated(data$`Order Id`) | duplicated(data$`Order Id`, fromLast = TRUE)) & data$`Unit Price`> 0 & data$`Total Payment`> 0 & data$`Unit Price` <= data$`Total Payment`]
+bundle2 <- bundle2[bundle2$`Order Id` %in% non_use]
+  
+Final_Bundle <-
 
